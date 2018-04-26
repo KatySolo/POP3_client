e75cc282a29b3a9e28b47f3f5e9d5a263bee75a3
KatySolo <colo18@yandex.ru>
2018-04-26 22:08:00 +0500

    fixed output

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import base64
 import socket
 import ssl
-import re
+
 
 class AuthenticationError(Exception):
     pass
 
-def auth_user(socket,credentials):
+
+def auth_user(socket, credentials):
     for i in credentials:
         socket.send((i + '\r\n').encode())
         data = socket.recv(1024)
         if data.decode()[:2] == '-E':
             raise AuthenticationError
-        # print(data.decode())
-    print ("Connection established")
+            # print(data.decode())
+    print("Connection established")
+
 
 def establlish_connection():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,6 +24,7 @@ def establlish_connection():
     data = ssl_sock.recv(1024)
     # print(data.decode())
     return ssl_sock
+
 
 # ----1------
 #  =?UTF-8?B?0K/QvdC00LXQutGBLtCU0LXQvdGM0LPQuA==?= <inform@money.yandex.ru>
@@ -131,25 +134,23 @@ def encode_subject(data):
                     break
         return subject_data
     else:
-        return data[0].lstrip()[9:]
+        return data[0].lstrip()[9:].rstrip('\r')
 
 
 def get_sender_and_subject(data):
-    # TODO normal parce
     sender, address = encode_sender(data[:2])
     subject = '<no subject>'
-    # print (address)
-    for i in range(2,len(data)):
+    for i in range(2, len(data)):
         if data[i].startswith('Subject:'):
-            subject += encode_subject(data[i:])
+            subject = encode_subject(data[i:])
             break
-    return (sender,address, subject)
+    return (sender, address, subject)
 
 
 def encode_sender(data):
-    sender_data=''
+    sender_data = ''
     if data[0].lstrip().startswith('='):
-        for i in range (2):
+        for i in range(2):
             if i == 0:
                 sender_data += data[i].lstrip().rstrip('\r')
             elif i == 1:
@@ -159,59 +160,51 @@ def encode_sender(data):
                     continue
         splitted_data = sender_data.split(' ')
         a = splitted_data[0][10:-2]
-        email = splitted_data[1].rstrip('\r').replace('(','<').replace(')','>')
-        return (base64.b64decode(a).decode('utf-8'),email)
+        email = splitted_data[1].rstrip('\r').replace('(', '<').replace(')', '>')
+        return (base64.b64decode(a).decode('utf-8'), email)
     else:
-        a = data[0].lstrip().rstrip('\r').replace('(','<').replace(')','>')
+        a = data[0].lstrip().rstrip('\r').replace('(', '<').replace(')', '>')
         if a.index('<') == -1:
-            return (a.split('(')[0].rstrip(),'<'+a.split('(')[1].replace(')','>'))
-        return (a.split('<')[0].rstrip(),'<'+a.split('<')[1].replace(')','>'))
-
-
-
+            return (a.split('(')[0].rstrip(), '<' + a.split('(')[1].replace(')', '>'))
+        return (a.split('<')[0].rstrip(), '<' + a.split('<')[1].replace(')', '>'))
 
 
 def list_messages(socket, data_count):
-    # from_pattern = re.compile(r"\a+From: \a+<\a+>")
-    # subject_pattern = re.compile(r"\w+Subject: \w+")
-
-    #From: =?UTF-8?B?0JrQvtC80LDQvdC00LAg0J/QvtGH0YLRiyBNYWlsLlJ1?=<welcome@corp.mail.ru>
-    #Subject: =?UTF-8?B?0JrQsNC6INCy0L7RgdC/0L7Qu9GM0LfQvtCy0LDRgtGM0YHRjyDQv9C+?==?UTF-8?B?0YfRgtC+0Lkg0YEg0LzQvtCx0LjQu9GM0L3QvtCz0L4/?=
-    # \r\n.\r\n
-    for i in range(1,data_count+1):
-        print ('----'+str(i)+'------')
+    for i in range(1, data_count + 1):
+        # print ('----'+str(i)+'----')
         socket.send(("TOP " + str(i) + " 0" + '\r\n').encode())
         data = ""
         raw_data = ''
         while not raw_data.startswith('.') and not raw_data.startswith('\r\n'):
             raw_data = socket.recv(1024).decode()
             if raw_data[:2] == '-E':
-                # print (raw_data)
                 break
             data += raw_data
         from_field = data.split('\nFrom:')[1].split("\n")[:7]
         sender, address, subject = get_sender_and_subject(from_field)
-        print (sender, address, subject)
+        print('({0})---{3} | {1} {2}'.format(str(i), sender, address, subject))
+
 
 def show_inbox(socket):
     socket.send(("LIST" + '\r\n').encode())
-    data=""
+    data = ""
     raw_data = ''
     while raw_data[-3:] != '.\r\n':
         raw_data = socket.recv(1024).decode()
         data += raw_data
-    data_count = len(data.split('\r\n'))-3
-    print ("You have "+ str(data_count) + " messages in your mailbox")
+    data_count = len(data.split('\r\n')) - 3
+    print("You have " + str(data_count) + " messages in your mailbox")
     list_messages(socket, data_count)
 
     # print (data)
+
 
 if __name__ == '__main__':
     email = 'test.katy.solo@yandex.ru'
     password = 'qwertyKate98'
     try:
         socket = establlish_connection()
-        auth_user(socket,['USER '+email,'PASS '+password])
+        auth_user(socket, ['USER ' + email, 'PASS ' + password])
         show_inbox(socket)
     except AuthenticationError:
         print('Wrong login/password')
